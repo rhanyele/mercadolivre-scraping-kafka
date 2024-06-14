3d858c3130db1e6d460b99ccedb7742d85ed668a
Rhanyele Marinho <rhany.marinho@hotmail.com>
2024-06-14 11:28:25 -0400

    Atualização do docker compose e configuração do kafka

--- src/config/kafka_config.py
+++ src/config/kafka_config.py
@@ -1,6 +1,6 @@
 # config/kafka_config.py
 
-KAFKA_BROKER_URL = 'localhost:9092'
+KAFKA_BROKER_URL = 'localhost:29092'
 KAFKA_OFFER_TOPIC = 'mercadolivre_offers'
 KAFKA_DISCOUNT_TOPIC = 'mercadolivre_offers_discounts'
 KAFKA_OFFER_GROUP = 'offer_discount_group'
